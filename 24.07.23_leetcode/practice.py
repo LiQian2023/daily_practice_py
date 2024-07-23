c2fef48a90f4c11fc6da4829cbdb50745ca42334
Li_2023-9-13 <1476021453@qq.com>
2024-07-23 23:42:46 +0800

    2024.07.23力扣网刷题 第三大的数——数组、排序——简单 方法一：列表去重加排序

--- 24.07.23_leetcode/practice.py
+++ 24.07.23_leetcode/practice.py
@@ -28,7 +28,7 @@
 # - 10^9 <= nums1[i], nums2[j] <= 10^9
 # 进阶：你可以设计实现一个时间复杂度为 O(m + n) 的算法解决此问题吗？
 
-class Solution(object):
+class Solution1(object):
     def merge1(self, nums1, m, nums2, n):
         """
         :type nums1: List[int]
@@ -72,7 +72,6 @@ class Solution(object):
             nums1[i] = tmp[i]
         print(tmp)
 
-
     def merge(self, nums1, m, nums2, n):
         """
         :type nums1: List[int]
@@ -97,6 +96,37 @@ class Solution(object):
             nums1[len] = nums2[j]
             len -= 1
             j -= 1
+
+# 第三大的数——数组、排序——简单
+# 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
+# 示例 1：
+# 输入：[3, 2, 1]
+# 输出：1
+# 解释：第三大的数是 1 。
+# 示例 2：
+# 输入：[1, 2]
+# 输出：2
+# 解释：第三大的数不存在, 所以返回最大的数 2 。
+# 示例 3：
+# 输入：[2, 2, 3, 1]
+# 输出：1
+# 解释：注意，要求返回第三大的数，是指在所有不同数字中排第三大的数。
+# 此例中存在两个值为 2 的数，它们都排第二。在所有不同数字中排第三大的数为 1 。
+# 提示：
+# 1 <= nums.length <= 10^4
+# - 2^31 <= nums[i] <= 2^31 - 1
+# 进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        one = list(set(nums))
+        one.sort(reverse=True)
+        if len(one) < 3:
+            return max(one)
+        return one[2]
 
 
 nums1 = [1,2,3,0,0,0]
