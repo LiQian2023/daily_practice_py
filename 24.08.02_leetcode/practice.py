cd111b6f52d33bf7d07423a68746302bf7f312cb
Li_2023-9-13 <1476021453@qq.com>
2024-08-02 00:58:44 +0800

    2024.08.02力扣网刷题 两点之间不包含任何点的最宽垂直区域——数组、排序——简单 方法一：sort排序

---
+++ 24.08.02_leetcode/practice.py
@@ -0,0 +1,28 @@
+# 2024.08.02力扣网刷题
+# 两点之间不包含任何点的最宽垂直区域——数组、排序——简单
+# 给你 n 个二维平面上的点 points ，其中 points[i] = [xi, yi] ，请你返回两点之间内部不包含任何点的 最宽垂直区域 的宽度。
+# 垂直区域 的定义是固定宽度，而 y 轴上无限延伸的一块区域（也就是高度为无穷大）。 最宽垂直区域 为宽度最大的一个垂直区域。
+# 请注意，垂直区域 边上 的点 不在 区域内。
+# 示例 1：
+# 输入：points = [[8, 7], [9, 9], [7, 4], [9, 7]]
+# 输出：1
+# 解释：红色区域和蓝色区域都是最优区域。
+# 示例 2：
+# 输入：points = [[3, 1], [9, 0], [1, 0], [1, 4], [5, 3], [8, 8]]
+# 输出：3
+# 提示：
+# n == points.length
+# 2 <= n <= 10^5
+# points[i].length == 2
+# 0 <= xi, yi <= 10^9
+
+
+class Solution(object):
+    def maxWidthOfVerticalArea(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        # 方法一：sort排序
+        points.sort(key=lambda x: x[0])
+        return max([points[i][0] - points[i - 1][0] for i in range(1, len(points))])
