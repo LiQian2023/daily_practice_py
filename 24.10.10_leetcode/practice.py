2cece2070aef1c9e30dd1f9d3974d70cbf3c6d3c
Li_2023-9-13 <1476021453@qq.com>
2024-10-10 01:15:41 +0800

    2024.10.10力扣网刷题 优质数对的总数 I——数组、哈希表——简单 方法一：暴力求解

---
+++ 24.10.10_leetcode/practice.py
@@ -0,0 +1,39 @@
+# 2024.10.10力扣网刷题
+# 优质数对的总数 I——数组、哈希表——简单
+# 给你两个整数数组 nums1 和 nums2，长度分别为 n 和 m。同时给你一个正整数 k。
+# 如果 nums1[i] 可以被 nums2[j] * k 整除，则称数对(i, j) 为 优质数对（0 <= i <= n - 1, 0 <= j <= m - 1）。
+# 返回 优质数对 的总数。
+# 示例 1：
+# 输入：nums1 = [1, 3, 4], nums2 = [1, 3, 4], k = 1
+# 输出：5
+# 解释：
+# 5个优质数对分别是(0, 0), (1, 0), (1, 1), (2, 0), 和(2, 2)。
+# 示例 2：
+# 输入：nums1 = [1, 2, 4, 12], nums2 = [2, 4], k = 3
+# 输出：2
+# 解释：
+# 2个优质数对分别是(3, 0) 和(3, 1)。
+# 提示：
+# 1 <= n, m <= 50
+# 1 <= nums1[i], nums2[j] <= 50
+# 1 <= k <= 50
+
+
+class Solution(object):
+    def numberOfPairs(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: int
+        """
+        length1 = len(nums1)
+        length2 = len(nums2)
+        for i in range(length2):
+            nums2[i] *= k
+        ans = 0
+        for i in range(length1):
+            for j in range(length2):
+                if nums1[i] % nums2[j] == 0:
+                    ans += 1
+        return ans
