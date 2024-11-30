8fe7ec7551eb16e9ef376d2dc023957029807840
Li_2023-9-13 <1476021453@qq.com>
2024-11-30 16:13:15 +0800

    2024.11.30力扣网刷题 判断是否可以赢得数字游戏——数组、数学——简单 方法一：数组遍历

---
+++ 24.11.30_leetcode/practice.py
@@ -0,0 +1,42 @@
+# 2024.11.30力扣网刷题
+# 判断是否可以赢得数字游戏——数组、数学——简单
+# 给你一个 正整数 数组 nums。
+# Alice 和 Bob 正在玩游戏。在游戏中，Alice 可以从 nums 中选择所有个位数 或 所有两位数，剩余的数字归 Bob 所有。如果 Alice 所选数字之和 严格大于 Bob 的数字之和，则 Alice 获胜。
+# 如果 Alice 能赢得这场游戏，返回 true；否则，返回 false。
+# 示例 1：
+# 输入：nums = [1, 2, 3, 4, 10]
+# 输出：false
+# 解释：
+# Alice 不管选个位数还是两位数都无法赢得比赛。
+# 示例 2：
+# 输入：nums = [1, 2, 3, 4, 5, 14]
+# 输出：true
+# 解释：
+# Alice 选择个位数可以赢得比赛，所选数字之和为 15。
+# 示例 3：
+# 输入：nums = [5, 5, 5, 25]
+# 输出：true
+# 解释：
+# Alice 选择两位数可以赢得比赛，所选数字之和为 25。
+# 提示：
+# 1 <= nums.length <= 100
+# 1 <= nums[i] <= 99
+
+class Solution(object):
+    def canAliceWin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        alice1, alice2, bob1, bob2 = 0, 0, 0, 0
+        length = len(nums)
+        for i in range(length):
+            if nums[i] < 10:
+                alice1 += nums[i]
+            else:
+                bob1 += nums[i]
+            if 9 < nums[i] < 100:
+                alice2 += nums[i]
+            else:
+                bob2 += nums[i]
+        return alice1 - bob1 > 0 or alice2 - bob2 > 0
