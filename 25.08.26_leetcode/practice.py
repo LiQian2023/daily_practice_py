9fa63497f4ada5eb49e812d14d105223f9bce0d3
Li_2023-9-13 <1476021453@qq.com>
2025-08-26 04:54:50 +0800

    2025.08.26力扣网刷题 对角线最长的矩形的面积——数组、第379场周赛——简单 方法一：数组遍历

---
+++ 25.08.26_leetcode/practice.py
@@ -0,0 +1,38 @@
+# 2025.08.26力扣网刷题
+# 对角线最长的矩形的面积——数组、第379场周赛——简单
+# 给你一个下标从 0 开始的二维整数数组 dimensions。
+# 对于所有下标 i（0 <= i < dimensions.length），dimensions[i][0] 表示矩形 i 的长度，而 dimensions[i][1] 表示矩形 i 的宽度。
+# 返回对角线最 长 的矩形的 面积 。如果存在多个对角线长度相同的矩形，返回面积最 大 的矩形的面积。
+# 示例 1：
+# 输入：dimensions = [[9, 3], [8, 6]]
+# 输出：48
+# 解释：
+# 下标 = 0，长度 = 9，宽度 = 3。对角线长度 = sqrt(9 * 9 + 3 * 3) = sqrt(90) ≈ 9.487。
+# 下标 = 1，长度 = 8，宽度 = 6。对角线长度 = sqrt(8 * 8 + 6 * 6) = sqrt(100) = 10。
+# 因此，下标为 1 的矩形对角线更长，所以返回面积 = 8 * 6 = 48。
+# 示例 2：
+# 输入：dimensions = [[3, 4], [4, 3]]
+# 输出：12
+# 解释：两个矩形的对角线长度相同，为 5，所以最大面积 = 12。
+# 提示：
+# 1 <= dimensions.length <= 100
+# dimensions[i].length == 2
+# 1 <= dimensions[i][0], dimensions[i][1] <= 100
+
+class Solution(object):
+    def areaOfMaxDiagonal(self, dimensions):
+        """
+        :type dimensions: List[List[int]]
+        :rtype: int
+        """
+        ans = 0
+        tmp = 0
+        for dimension in dimensions:
+            l = (dimension[0] ** 2 + dimension[1] ** 2) ** 0.5
+            s = dimension[0] * dimension[1]
+            if l > tmp:
+                tmp = l
+                ans = s
+            elif l == tmp and ans < s:
+                ans = s
+        return ans
