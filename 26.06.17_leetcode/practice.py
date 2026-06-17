5eac78725bb764be2ab5c09245fbb034a544c8a6
Noah <1476021453@qq.com>
2026-06-17 18:40:40 +0800

    2026.06.17力扣网刷题 3959. 判定好整数——中级工程师、第506场周赛——简单 方法一：模拟

---
+++ 26.06.17_leetcode/practice.py
@@ -0,0 +1,34 @@
+# 2026.06.17力扣网刷题
+# 3959. 判定好整数——中级工程师、第506场周赛——简单
+# 给你一个正整数 n。
+# 令 digitSum 表示 n 的各位数字之和，令 squareSum 表示 n 的各位数字平方之和。
+# 如果一个整数满足 squareSum - digitSum >= 50，则称它是 好整数 。
+# 如果 n 是好整数，返回 true；否则，返回 false。
+# 示例 1：
+# 输入： n = 1000
+# 输出： false
+# 解释：
+# 1000 的数字为 1、0、0 和 0。
+# digitSum 为 1 + 0 + 0 + 0 = 1。
+# squareSum 为 12 + 02 + 02 + 02 = 1。
+# squareSum - digitSum 为 1 - 1 = 0。由于 0 小于 50，因此输出 false。
+# 示例 2：
+# 输入： n = 19
+# 输出： true
+# 解释：
+# 19 的数字为 1 和 9。
+# digitSum 为 1 + 9 = 10。
+# squareSum 为 12 + 92 = 1 + 81 = 82。
+# squareSum - digitSum 为 82 - 10 = 72。由于 72 大于等于 50，因此输出 true。
+# 提示：
+# 1 <= n <= 10^9
+
+class Solution:
+    def checkGoodInteger(self, n: int) -> bool:
+        sq, di = 0, 0
+        while n:
+            x = n % 10
+            n //= 10
+            sq += x ** 2
+            di += x
+        return sq - di >= 50
