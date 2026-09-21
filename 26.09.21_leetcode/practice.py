58f90044ced58f97f64cd3bab32a153536da074d
Noah <1476021453@qq.com>
2026-09-21 20:14:54 +0800

    2026.09.21力扣网刷题 89. 格雷编码——位运算、数学、回溯——中等 方法一：回溯 方法二：位运算

---
+++ 26.09.21_leetcode/practice.py
@@ -0,0 +1,63 @@
+# 2026.09.21力扣网刷题
+# 89. 格雷编码——位运算、数学、回溯——中等
+# n 位格雷码序列 是一个由 2n 个整数组成的序列，其中：
+# 每个整数都在范围[0, 2n - 1] 内（含 0 和 2n - 1）
+# 第一个整数是 0
+# 一个整数在序列中出现 不超过一次
+# 每对 相邻 整数的二进制表示 恰好一位不同 ，且
+# 第一个 和 最后一个 整数的二进制表示 恰好一位不同
+# 给你一个整数 n ，返回任一有效的 n 位格雷码序列 。
+# 示例 1：
+# 输入：n = 2
+# 输出：[0, 1, 3, 2]
+# 解释：
+# [0, 1, 3, 2] 的二进制表示是[00, 01, 11, 10] 。
+# - 00 和 01 有一位不同
+# - 01 和 11 有一位不同
+# - 11 和 10 有一位不同
+# - 10 和 00 有一位不同
+# [0, 2, 3, 1] 也是一个有效的格雷码序列，其二进制表示是[00, 10, 11, 01] 。
+# - 00 和 10 有一位不同
+# - 10 和 11 有一位不同
+# - 11 和 01 有一位不同
+# - 01 和 00 有一位不同
+# 示例 2：
+# 输入：n = 1
+# 输出：[0, 1]
+# 提示：
+# 1 <= n <= 16
+
+class Solution:
+    def dfs(self, n, visited, ans, num):
+        for i in range(n):
+            num ^= 1 << i
+            if visited[num] == False:
+                visited[num] = True
+                ans.append(num)
+                self.dfs(n, visited, ans, num)
+            num ^= 1 << i
+        
+    def grayCode1(self, n: int) -> list[int]:
+        size = 1 << n
+        visited = [False] * size
+        visited[0] = True
+        ans = [0]
+        self.dfs(n, visited, ans, 0)
+        return ans
+    
+
+    def grayCode2(self, n: int) -> list[int]:
+        ans, size = [], 1 << n
+        visited = [False] * size
+        for i in range(size):
+            num = i ^ (i >> 1)
+            if visited[num] == False:
+                visited[num] = True
+                ans.append(num)
+        return ans
+
+    def grayCode(self, n: int) -> list[int]:
+        ans, size = [], 1 << n
+        for i in range(size):
+            ans.append(i ^ (i >> 1))
+        return ans
